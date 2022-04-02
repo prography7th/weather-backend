bcdc95061dc22459365205561752dedf39a47400
Haneul Lee <hee1149kr@naver.com>
2022-04-02 22:54:41 +0900

    setting: project structure setting (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validationSchema } from '@config/validationSchema';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
 })
 export class AppModule {}
