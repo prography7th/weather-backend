053b968d6493cf876d3facedefa5b8ca0d87265c
Hanna Lee <8annahxxl@gmail.com>
2022-04-03 17:21:47 +0900

    etc: Dockerfile 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { validationSchema } from '@config/validationSchema';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 3306,
+      port: parseInt(process.env.DB_PORT) || 3306,
       username: process.env.DB_USERNAME || 'root',
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
