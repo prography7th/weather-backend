36b796f1dc0e6c0bdb540724180a00eef27728af
leesky <leehaneul0623@gmail.com>
2022-04-08 16:16:37 +0900

    feat: 3일 이후 중기 예보 구현

--- src/forecast/middle-forecast.service.spec.ts
+++ src/forecast/middle-forecast.service.spec.ts
@@ -5,7 +5,8 @@ import { MiddleForecastService } from './middle-forecast.service';
 
 describe('middle-forecast', () => {
   let middleForecastService: MiddleForecastService;
-  let sampleRegionCode: string;
+  let sampleRegionCode: string[];
+  let sampleDate: string;
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       imports: [ConfigModule.forRoot()],
@@ -13,43 +14,81 @@ describe('middle-forecast', () => {
     }).compile();
 
     middleForecastService = module.get<MiddleForecastService>(MiddleForecastService);
-    sampleRegionCode = await middleForecastService.getRegionCode(128.8875970893, 35.88795706523);
+    sampleRegionCode = await middleForecastService.getRegionCode(127.0275833, 37.4979278);
+    sampleDate = middleForecastService.makeDateFormat(new Date());
   });
 
   describe('날씨 요청 데이터 포멧 함수', () => {
-    it('', () => {});
+    it('6시 이전에 요청시 전날 18시 반환', async () => {
+      let result = middleForecastService.makeDateFormat(new Date(2022, 3, 8, 5));
+      expect(result).toBe('202204071800');
+    });
+
+    it('6시 이후에 요청시 당일 6시 반환', async () => {
+      let result = middleForecastService.makeDateFormat(new Date(2022, 3, 8, 7));
+      expect(result).toBe('202204080600');
+    });
+    it('18시 이후에 요청시 당일 18시 반환', async () => {
+      let result = middleForecastService.makeDateFormat(new Date(2022, 3, 7, 19));
+      expect(result).toBe('202204071800');
+    });
   });
 
   describe('중기 예보 API 요청', () => {
+    it('', async () => {
+      let result = await middleForecastService.getMiddleForecastInformation(127.0275833, 37.4979278);
+      //console.log(result);
+    });
+
     it('3일부터 10일까지 기온 정보 조회 정상작동', async () => {
-      let result = await middleForecastService.getTempMiddleForecast(sampleRegionCode);
+      let result = await middleForecastService.getTempMiddleForecast(sampleRegionCode[0], sampleDate);
       expect(result).toBeDefined();
-      console.log(result);
     });
 
     it('3일부터 10까지 육상 정보 조회 정상작동', async () => {
-      let result = await middleForecastService.getRandMiddleForecast(sampleRegionCode);
+      let result = await middleForecastService.getRandMiddleForecast(sampleRegionCode[1], sampleDate);
       expect(result).toBeDefined();
-      console.log(result);
     });
   });
 
   describe('좌표를 입력해서 얻은 행정구역으로 중기예보 지역코드 얻기', () => {
     it('영천시', async () => {
       let result = await middleForecastService.getRegionCode(128.8875970893, 35.88795706523);
-      expect(result).toBe('11H10702');
+      expect(result).toStrictEqual(['11H10702', '11H10000']);
     });
     it('안양시', async () => {
       let result = await middleForecastService.getRegionCode(126.9568209, 37.3942527);
-      expect(result).toBe('11B20602');
+      expect(result).toStrictEqual(['11B20602', '11B00000']);
     });
     it('서울특별시', async () => {
       let result = await middleForecastService.getRegionCode(126.986, 37.541);
-      expect(result).toBe('11B10101');
+      expect(result).toStrictEqual(['11B10101', '11B00000']);
     });
     it('세종특별자치시', async () => {
       let result = await middleForecastService.getRegionCode(127.2494855, 36.5040736);
-      expect(result).toBe('11C20404');
+      expect(result).toStrictEqual(['11C20404', '11C20000']);
+    });
+
+    describe('광주광역시 경기도 광주 판별', () => {
+      it('광주광역시', async () => {
+        let result = await middleForecastService.getRegionCode(126.8526012, 35.1595454);
+        expect(result).toStrictEqual(['11F20501', '11F20000']);
+      });
+      it('경기도 광주', async () => {
+        let result = await middleForecastService.getRegionCode(127.2526508, 37.399198);
+        expect(result).toStrictEqual(['11B20702', '11B00000']);
+      });
+    });
+
+    describe('강원도 영동지방 영서지방 판별', () => {
+      it('강릉시(영동)', async () => {
+        let result = await middleForecastService.getRegionCode(128.8760574, 37.751853);
+        expect(result).toStrictEqual(['11D20501', '11D20000']);
+      });
+      it('원주시(영서)', async () => {
+        let result = await middleForecastService.getRegionCode(127.9201621, 37.3422186);
+        expect(result).toStrictEqual(['11D10401', '11D10000']);
+      });
     });
   });
 });
--- src/forecast/middle-forecast.service.ts
+++ src/forecast/middle-forecast.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Coordinate } from 'coordinate-transformer';
-import { regionCode } from './middle-forecast.region-code';
+import { randRegionCode, yeongseoRegions, yeongdongRegions } from './rand.region-information';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
+import { regionCode } from './middle-forecast.region-code';
 @Injectable()
 export class MiddleForecastService {
   Coordinate: any;
@@ -10,6 +11,7 @@ export class MiddleForecastService {
   serviceKey: string;
   tempEndpoint: string;
   randEndpoint: string;
+  week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 
   constructor(private configService: ConfigService) {
     this.Coordinate = require('../lib/coordinateTransformer/src/index.js');
@@ -20,25 +22,126 @@ export class MiddleForecastService {
 
   async getMiddleForecastInformation(x: number, y: number) {
     const regionCode = await this.getRegionCode(x, y);
+    let requestDate = this.makeDateFormat(new Date());
+    let tmpResult = await this.getTempMiddleForecast(regionCode[0], requestDate);
+    let rndResult = await this.getRandMiddleForecast(regionCode[1], requestDate);
+
+    let year = requestDate.slice(0, 4);
+    let month = requestDate.slice(4, 6);
+    let day = requestDate.slice(6, 8);
+
+    delete tmpResult[0].regId;
+    let temperatures = Object.entries(tmpResult[0]).filter(
+      (entry) =>
+        !entry[0].includes('High') &&
+        !entry[0].includes('Low') &&
+        !entry[0].includes('8') &&
+        !entry[0].includes('9') &&
+        !entry[0].includes('10'),
+    );
+
+    delete rndResult[0].regId;
+    let precipitation = Object.entries(rndResult[0]).filter(
+      (entry) =>
+        !entry[0].includes('wf') && !entry[0].includes('8') && !entry[0].includes('9') && !entry[0].includes('10'),
+    );
+
+    let weather = Object.entries(rndResult[0]).filter(
+      (entry) =>
+        !entry[0].includes('rnSt') && !entry[0].includes('8') && !entry[0].includes('9') && !entry[0].includes('10'),
+    );
+
+    let result = {
+      requestDay: new Date(requestDate).getDay(),
+      informations: [],
+    };
+
+    for (let i = 0; i < 10; i += 2) {
+      result.informations.push({
+        day: this.week[new Date(Number(year), Number(month) - 1, Number(day + 3 + i / 2)).getDay()],
+        tmpMin: temperatures[i][1],
+        tmpMax: temperatures[i + 1][1],
+        am: {
+          precipitation: precipitation[i][1],
+          weather: weather[i][1],
+        },
+        pm: {
+          precipitation: precipitation[i + 1][1],
+          weather: weather[i + 1][1],
+        },
+      });
+    }
+
+    console.log(JSON.stringify(result, null, 2));
+    return result;
+    //오늘 기준 3일 이후 부터 강수확률, 최대 최저 기온, 날씨
+    /*
+      {
+        request_date: 
+        weather: {
+          1:
+          2: //여기가 3
+          3: 
+          
+        }
+      }
+
+
+    */
   }
 
-  async getTempMiddleForecast(regionCode: string) {
+  async getTempMiddleForecast(regionCode: string, date: string) {
     let result = await axios.get(
-      `${this.tempEndpoint}?serviceKey=${this.serviceKey}&pageNo=1&numOfRows=10&dataType=JSON&regId=${regionCode}&tmFc=202204070600`,
+      `${this.tempEndpoint}?serviceKey=${this.serviceKey}&pageNo=1&numOfRows=10&dataType=JSON&regId=${regionCode}&tmFc=${date}`,
     );
     return result.data.response.body.items.item;
   }
-  async getRandMiddleForecast(regionCode: string) {
-    console.log(new Date());
+
+  async getRandMiddleForecast(regionCode: string, date: string) {
     let result = await axios.get(
-      `${this.randEndpoint}?serviceKey=${this.serviceKey}&pageNo=1&numOfRows=10&dataType=JSON&regId=${regionCode}&tmFc=202204070600`,
+      `${this.randEndpoint}?serviceKey=${this.serviceKey}&pageNo=1&numOfRows=10&dataType=JSON&regId=${regionCode}&tmFc=${date}`,
     );
     return result.data.response.body.items.item;
+  }
+
+  makeDateFormat(date: Date) {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    if (hour < 6) {
+      hour = 18;
+      day = day - 1;
+    } else hour = 18 <= hour ? 18 : 6;
+
+    return `${
+      String(year) + (month < 10 ? '0' + month : month) + (day < 10 ? '0' + day : day) + (hour < 10 ? '0' + hour : hour)
+    }00`;
   }
 
   async getRegionCode(x: number, y: number) {
     this.ct = new this.Coordinate(x, y);
     let result: Coordinate.AllRegionInformationType = await this.ct.getResult();
-    return regionCode.filter((item) => result.documents[0].address_name.includes(item[0])).flat()[1];
+    let randCode;
+    let randRegionCodeKeys = Object.keys(randRegionCode);
+    if (result.documents[0].region_1depth_name === '강원도') {
+      yeongdongRegions.forEach((region) => {
+        if (result.documents[0].address_name.includes(region)) {
+          randCode = '11D20000';
+        }
+      });
+      randCode = randCode === '11D20000' ? randCode : '11D10000';
+    } else if (result.documents[0].region_1depth_name === '광주광역시') {
+      return ['11F20501', '11F20000'];
+    } else {
+      randRegionCodeKeys.forEach((regionCodeKey) => {
+        randRegionCode[regionCodeKey].forEach((region) => {
+          if (result.documents[0].address_name.includes(region)) randCode = regionCodeKey;
+        });
+      });
+    }
+
+    let tmpCode = regionCode.filter((item) => result.documents[0].address_name.includes(item[0])).flat()[1];
+    return [tmpCode, randCode];
   }
 }
