4a9862b90e6327d8aebdf31dc8a4dc3226824693
Hanna Lee <8annahxxl@gmail.com>
2022-04-14 23:12:01 +0900

    docs: 스웨거 tag, summary, 유저 프로퍼티 description 작성

--- src/users/entity/user.entity.ts
+++ src/users/entity/user.entity.ts
@@ -1,24 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, IsUUID } from 'class-validator';
 import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('User')
 export class UserEntity {
+  @ApiProperty({ description: 'UUID' })
   @IsUUID()
   @PrimaryColumn({ length: 40 })
   id: string;
 
+  @ApiProperty({ description: '디바이스 토큰' })
   @IsString()
   @Column({ nullable: false, length: 255 })
   token: string;
 
+  @ApiProperty({ description: '유저 위치 위도' })
   @IsString()
   @Column({ nullable: false, length: 30 })
   lat: string;
 
+  @ApiProperty({ description: '유저 위치 경도' })
   @IsString()
   @Column({ nullable: false, length: 30 })
   lon: string;
 
+  @ApiProperty({ description: '알람 시간 (0 ~ 24)' })
   @IsNumber()
   @Column({ default: null, type: 'tinyint' })
   alarmTime: number;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SaveUserDto } from './dto/save-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('User')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOperation({ summary: '유저 정보 조회' })
   @Get(':id')
   getUser(@Param('id') id: string) {
     return this.usersService.getUser(id);
   }
 
+  @ApiOperation({ summary: '유저 정보 등록' })
   @Post()
   saveUser(@Body() body: SaveUserDto) {
     const { id, token, lat, lon } = body;
@@ -19,6 +23,7 @@ export class UsersController {
     return this.usersService.saveUser(id, token, lat, lon);
   }
 
+  @ApiOperation({ summary: '유저 정보 수정' })
   @Put(':id')
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     const { lat, lon, alarmTime } = body;
@@ -26,6 +31,7 @@ export class UsersController {
     return this.usersService.updateUser(id, lat, lon, alarmTime);
   }
 
+  @ApiOperation({ summary: '유저 정보 삭제' })
   @Delete(':id')
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
