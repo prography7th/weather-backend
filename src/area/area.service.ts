d0b99ff3613762889c53ff52abb635c4aa63c95e
Hanna Lee <8annahxxl@gmail.com>
2022-05-03 03:56:43 +0900

    deploy: 작업 내용 배포 (#48)
    * fix: --name 추가
    
    * fix: 컨테이너 name 변경
    
    * fix: 컨테이너 name 변경
    
    * Update main.yml
    
    * feat: 유저 CRUD 구현 (#29)
    
    * etc: orm sync 변수화
    
    * feat: SuccessInterceptor 등록
    
    * feat: 유저 CRUD 기능 구현
    
    * docs: 스웨거 tag, summary, 유저 프로퍼티 description 작성
    
    * feat: 알람 테이블 분리하여 알람 시간 add, remove 기능 구현 (#31)
    
    * feat: 알람 시간 seeding (#35)
    
    * feat: 행정구역 모듈 구현 (#36)
    
    * feat: 행정구역 seeding
    
    * refactor: seeder 클래스 이름 클래스명 수정
    
    * feat: 유저 정보 행정구역 저장하도록 Entity와 서비스 로직 변경
    
    * docs: API 응답 문서 작성 (#38)
    
    * fix: prefix 값 Swagger 문서에 나타나도록 수정
    
    * feat: DTO 응답 데코레이터 구현
    
    * feat: 응답 DTO 작성 후 데코레이터에 적용
    
    * docs: properties 예시 값 추가
    
    * etc: DB sync 옵션 기본값 추가
    
    * fix: now baseDate, baseTime 잘못된 값 들어가는 버그 수정 (#43)
    
    * refactor: DMS 변환 함수 AreaService 메서드로 이동
    
    * feat: 단기예보 PTY(강수형태)값 추가 (#47)
    
    * feat: 단기예보 요청 부분 500 에러 처리
    
    * feat: 단기예보 pty값 추가

--- src/area/area.service.ts
+++ src/area/area.service.ts
@@ -4,21 +4,22 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AreaEntity } from './entity/area.entity';
 
-function convertLatOrLonToDMS(value: number | string) {
-  value = Number(value);
-  const D = Math.floor(value);
-  const M = Math.floor((value - D) * 60);
-  const S = ((value - D) * 60 - M) * 60;
-
-  return { D, M, S };
-}
-
 @Injectable()
 export class AreaService {
   constructor(@InjectRepository(AreaEntity) private areaRepository: Repository<AreaEntity>) {}
+
+  private convertLatOrLonToDMS(value: number | string) {
+    value = Number(value);
+    const D = Math.floor(value);
+    const M = Math.floor((value - D) * 60);
+    const S = ((value - D) * 60 - M) * 60;
+
+    return { D, M, S };
+  }
+
   async getArea(lat: string, lon: string): Promise<AreaEntity[]> {
-    const latDMS = convertLatOrLonToDMS(lat);
-    const lonDMS = convertLatOrLonToDMS(lon);
+    const latDMS = this.convertLatOrLonToDMS(lat);
+    const lonDMS = this.convertLatOrLonToDMS(lon);
     const { x, y } = dfs_xy_conv('toXY', lat, lon);
 
     return await this.areaRepository.find({
