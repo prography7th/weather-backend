bece892b1910a4632c3564e42f6c44d83e97922e
leesky <leehaneul0623@gmail.com>
2022-07-10 23:00:19 +0900

    feat: base를 캐시해야하는 날인지 알 수 있도록 구현

--- src/produce/produce.service.ts
+++ src/produce/produce.service.ts
@@ -1,16 +1,18 @@
-import { AreaService } from '@app/area/area.service';
 import { AreaEntity } from '@app/area/entity/area.entity';
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SqsService } from '@ssut/nestjs-sqs';
 import { Repository } from 'typeorm';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class ProduceService {
   private cachedGrid: Set<string>;
+  private isBaseContent: string;
   private produceTimes: number[] = [2, 5, 8, 11, 14, 17, 20, 23];
   constructor(
     private readonly sqsService: SqsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
     @InjectRepository(AreaEntity) private readonly areaRepository: Repository<AreaEntity>,
   ) {
     this.cachedGrid = new Set<string>();
@@ -18,7 +20,38 @@ export class ProduceService {
 
   @Cron('13 2,5,8,11,14,17,20,23 * * *', { name: 'sendEvent' })
   handleCron() {
+    this.setIsBaseContent();
     this.sendEvent();
+  }
+
+  /**
+   * @description
+   *  produce 하는 메시지가 base content가 될 수 있는지 확인한다.
+   *  produce 배치가 돌기전 최초 1회 실행된다.
+   *  return  "1(true)" or "0(false)"
+   */
+  private async setIsBaseContent(): Promise<void> {
+    await this.setRotateCountWithOne();
+    const now = await this.getRotateCount();
+    this.isBaseContent = now === 1 ? '1' : '0';
+    this.addRotateCount();
+  }
+
+  private async setRotateCountWithOne() {
+    const now = await this.getRotateCount();
+    if (now == null || now === 4) {
+      this.cacheManager.set<number>('rotateCount', 1);
+    }
+  }
+
+  private async addRotateCount() {
+    this.cacheManager.set<number>('rotateCount', (await this.getRotateCount()) + 1, {
+      ttl: 60 * 60 * 25,
+    });
+  }
+
+  private async getRotateCount(): Promise<number> {
+    return await this.cacheManager.get<number>('rotateCount');
   }
 
   private async sendEvent(): Promise<void> {
@@ -42,6 +75,7 @@ export class ProduceService {
               test: process.env.NODE_ENV === 'development' ? true : false,
               data: {
                 ...area,
+                isBaseContent: this.isBaseContent,
                 baseDate,
                 baseTime,
               },
